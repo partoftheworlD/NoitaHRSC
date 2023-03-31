9019d1066f83308c138f693ae7f93cd905c9cd33
partoftheworlD <partoftheworld@vivaldi.net>
2023-03-31 22:35:53 +0300

    fix 9d4e260

--- NoitaHRSC.ps1
+++ NoitaHRSC.ps1
@@ -10,6 +10,7 @@ function Generate-Message {
     Set-Location -Path C:\Users\$env:UserName\AppData\LocalLow\Nolla_Games_Noita\save00\stats\sessions
     $last_session = (Get-ChildItem . | Sort-Object -Descending -Property LastWriteTime | Select-Object -First 1).name
     $seed = Select-String -Path $last_session -Pattern 'world_seed="\d+"' | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } | select-object -First 1
+    if (!$seed) {return "Saving stats"}
     $play_time = Select-String -Path $last_session -Pattern 'playtime_Str=".*?"' | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } | select-object -First 1
     $message = "$seed $play_time".Replace("str", "").Replace("=", ": ").Replace('"', "").Replace("_", " ").Replace(" : ", ": ")
     return $message
@@ -22,5 +23,5 @@ function cd-save00 {
 Disable-Mods
 cd-save00
 git add .
-git commit -v --allow-empty -am $(Generate-Message)
+git commit -am $(Generate-Message)
 git push -f
